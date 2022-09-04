7675c798792da49d56f2373d6297e6b7ab5212a0
노호준 <hh950131@gmail.com>
2022-09-04 13:52:26 +0900

    스프링 입문 - 6. 스프링 DB 접근 기술

--- hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ hello-spring/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -1,19 +1,17 @@
 package hello.hellospring.service;
 
 import hello.hellospring.domain.Member;
-import hello.hellospring.repository.MemberReposiroty;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import hello.hellospring.repository.MemberRepository;
 
 import java.util.List;
 import java.util.Optional;
 
 
 public class MemberService {
-    private final MemberReposiroty memberReposiroty;
+    private final MemberRepository memberReposiroty;
 
 
-    public MemberService(MemberReposiroty memberReposiroty) {
+    public MemberService(MemberRepository memberReposiroty) {
         this.memberReposiroty = memberReposiroty;
     }
 
